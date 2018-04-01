69e9efd0da4688cc5c9e78bbf12998071fd36ba4
Shagun Verma <shagun.verma2101@gmail.com>
2018-04-01 08:56:48 +0530

    Calendar added...UI remains.

---
+++ AdVok8/AdVok8/Libraries/JTCalendar/JTCalendarDay.m
@@ -0,0 +1,13 @@
+//
+//  JTCalendarDay.m
+//  Practice_Artist
+//
+//  Created by Assusoft_mac1 on 20/01/16.
+//  Copyright (c) 2016 Assurance Software Pvt Ltd. All rights reserved.
+//
+
+#import "JTCalendarDay.h"
+
+@implementation JTCalendarDay
+
+@end
